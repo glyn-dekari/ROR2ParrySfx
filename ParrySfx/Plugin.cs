7a94cccbfa920a38009eef629f476d4c6b62edba
glyn-dekari <xaviermbcalabia@gmail.com>
2025-12-03 13:11:41 +0800

    RiskOfOptions
    Basic RiskOfOptions support

--- ParrySfx/Plugin.cs
+++ ParrySfx/Plugin.cs
@@ -1,12 +1,17 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using R2API;
 using R2API.Utils;
 using RoR2;
+using RiskOfOptions;
+using RiskOfOptions.Options;
 
 namespace ParrySfx
 {
 
     [BepInDependency(SoundAPI.PluginGUID)]
+    [BepInDependency("com.rune500.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [NetworkCompatibility(CompatibilityLevel.NoNeedForSync)]
 
@@ -17,9 +22,22 @@ namespace ParrySfx
         public const string PluginName = "ParrySFX";
         public const string PluginVersion = "1.0.0";
 
+        public static ConfigEntry<string> ParrySoundType { get; set; }
+        public static ConfigEntry<float> Volume {  get; set; }
+
         public void Awake()
         {
             Log.Init(Logger);
+
+            ParrySoundType = Config.Bind("Parry SFX", "ParrySoundIndex", "3S", "Controls which sound effect to play on a successful parry.");
+            Volume = Config.Bind("Parry SFX", "Volume", 0.5f, "Controls the volume of the sound effect.");
+
+            if(BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+            {
+                //TODO: change this to use the enum
+                ModSettingsManager.AddOption(new StringInputFieldOption(ParrySoundType));
+                ModSettingsManager.AddOption(new SliderOption(Volume));
+            }
         }
 
         public void OnEnable() {
@@ -27,12 +45,27 @@ namespace ParrySfx
         }
 
         private void ParrySuccess(On.RoR2.HealthComponent.orig_TakeDamageProcess orig, HealthComponent self, DamageInfo damageInfo)
-        { 
+        {
+            string sfxType = ChooseParrySfx(ParrySoundType.Value);
             if (self.body.HasBuff(RoR2.DLC3Content.Buffs.Parrying))
             {
-                Util.PlaySound("ThirdStrike", self.gameObject);
+                Log.Info("[ParrySfx] Parry success, attempting to play sound on " + self.gameObject.ToString());
+                Util.PlaySound(sfxType, self.gameObject);
             }
             orig(self, damageInfo);
+        }
+
+        private string ChooseParrySfx(string sfxType)
+        {
+            //TODO: change this to use the enum
+            switch (sfxType) {
+                case "ThirdStrike":
+                    return "ParrySuccess";
+                case "MeltyBlood":
+                    return "ParryShield";
+                default:
+                    return "ParrySuccess";
+            }
         }
 
         public void OnDisable() { 
---
+++ ParrySfx/SfxEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParrySfx
+{
+    public enum SfxEnum
+    {
+        ThirdStrike,
+        MeltyBlood,
+        PizzaTower
+    }
+}
